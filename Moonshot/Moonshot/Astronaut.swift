fb928bf6a668a20a6c4371d64c0ccca95bd4428f
igibliss00 <igibliss00@gmail.com>
2020-07-21 19:40:56 -0400

    Initial commit for Moonshot

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by jc on 2020-07-21.
+//  Copyright © 2020 J. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Identifiable, Codable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by jc on 2020-07-21.
+//  Copyright © 2020 J. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    Image(mission.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
